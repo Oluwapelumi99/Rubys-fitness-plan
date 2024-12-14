7bdf195f510f7a2faebaa21a83aa917b17dd09da
Oluwapenlumi Fadare <oluwajosephine7@gmail.com>
2024-12-14 02:33:35 +0000

    Add the shop model and make migrations

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Shop(models.Model):
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    sizes = models.BooleanField(default=False, null=True, blank=True)
+    color = models.CharField(max_length=254, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
