ac64bb48e6039b874d8908870046c02bda7fe102
Oluwapenlumi Fadare <oluwajosephine7@gmail.com>
2024-12-15 19:46:30 +0000

    Add the search function to search through all products

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Shop
+from .models import Shop, Category
 
 # Register your models here.
-
+admin.site.register(Category)
 admin.site.register(Shop)
--- shop/views.py
+++ shop/views.py
@@ -1,12 +1,27 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Shop
 # Create your views here.
 
 def view_shop(request):
-    """ A view to return the shopping page """
+    """ A view to return all shopping list,  including sorting and search queries  """
+
     shops = Shop.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Please enter a search criteria!")
+                return redirect(reverse('shop'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            shops = shops.filter(queries)
     context = {
-        'shops': shops
+        'shops': shops,
+        'search_term': query,
     }
     return render(request, 'shop/shop.html', context)
 
