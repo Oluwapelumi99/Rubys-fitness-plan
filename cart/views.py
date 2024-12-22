d6ea9892320061407c6ce5d3b7b051eee860ed54
Oluwapenlumi Fadare <oluwajosephine7@gmail.com>
2024-12-22 04:53:25 +0000

    Add view to update bag quantity and update the price accordingly

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from shop.models import Shop
 
 
 
@@ -10,7 +12,7 @@ def view_cart(request):
     return render(request, 'cart/cart.html')
 
 def add_to_cart(request, item_id):
-    """ Add a quantity of the specified product to the shopping bag """
+    """ Add quantity ans sizes to the cart"""
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -35,4 +37,35 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+def update_cart(request, item_id):
+    """Update the quantity of the cart and update the price accordingly"""
+
+    shop = get_object_or_404(Shop, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'shop_size' in request.POST:
+        size = request.POST['shop_size']
+    cart = request.session.get('cart', {})
+
+    if size:
+        if quantity > 0:
+            cart[item_id]['items_by_size'][size] = quantity
+            messages.success(request, f'Updated size {size} {shop.name} quantity to {cart[item_id]["items_by_size"][size]}')
+        else:
+            del cart[item_id]['items_by_size'][size]
+            if not cart[item_id]['items_by_size']:
+                cart.pop(item_id)
+            messages.success(request, f'Removed size {size} {shop.name} from your cart')
+    else:
+        if quantity > 0:
+            cart[item_id] = quantity
+            messages.success(request, f'Updated {shop.name} quantity to {cart[item_id]}')
+        else:
+            cart.pop(item_id)
+            messages.success(request, f'Removed {shop.name} from your cart')
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
     
