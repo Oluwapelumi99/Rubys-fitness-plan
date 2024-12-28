b4d75ffbce1722925290fdd0d07d32795ae8e02d
Oluwapenlumi Fadare <oluwajosephine7@gmail.com>
2024-12-28 01:06:57 +0000

    Add the comments form to the community blog and add the create comment view

--- community/views.py
+++ community/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from .models import Blog, Comment
+# from .forms import CommentForm
 
 # Create your views here.
 
@@ -13,4 +16,25 @@ def community(request, slug):
     """ A view to return the community page """
     queryset = Blog.objects.all()
     blog = get_object_or_404(queryset, slug=slug)
-    return render(request, 'community/community.html', {'blog': blog})
+    comments = blog.comments.all().order_by("-created_on")
+    comment_count = blog.comments.filter(approved=True).count()
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            blog = comment.blog
+            comment.save()
+            messages.add_message(
+        request, messages.SUCCESS,
+        'Comment submitted and awaiting approval'
+    )
+
+    comment_form = CommentForm()
+
+    return render(request, 'community/community.html', 
+    {'blog': blog,
+    "comments": comments,
+    "comment_count": comment_count,
+    "comment_form": comment_form,})
