f63f4fb2dfcd377e498767831fc73cef6b07eab6
Oluwapelumi99 <156908824+Oluwapelumi99@users.noreply.github.com>
2025-01-03 00:19:00 +0000

    Delete exercises directory

--- exercises/urls.py
+++
@@ -1,8 +0,0 @@
-# from . import views
-# from django.urls import path
-
-# urlpatterns = [
-#     path('exercise_page/', views.exercise_page, name='exercise_page'),
-#     path('glutes_exercises/', views.glutes_exercises, name='glutes_exercises'),
-#     path('abs_exercises/', views.abs_exercises, name='abs_exercises'),
-# ]
