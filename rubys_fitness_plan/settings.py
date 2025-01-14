670745f0a416c76179ef9b55e4c4585282961886
Oluwapenlumi Fadare <oluwajosephine7@gmail.com>
2025-01-14 11:35:08 +0000

    Final commit

--- rubys_fitness_plan/settings.py
+++ rubys_fitness_plan/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-oluwapelumi-rubysfitnes-5xbxqzqx0dv.ws.codeinstitute-ide.net',
                 'rubys-fitness-plan-f9d280e18cb5.herokuapp.com', 'localhost']
