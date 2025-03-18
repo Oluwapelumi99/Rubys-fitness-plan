25b68835da2ea4101172175168e4634571024b9c
Oluwapelumi Fadare <oluwajosephine7@gmail.com>
2025-03-18 22:00:10 +0000

    Add seo to html and meta tags

--- rubys_fitness_plan/settings.py
+++ rubys_fitness_plan/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-oluwapelumi-rubysfitnes-5xbxqzqx0dv.ws.codeinstitute-ide.net',
+ALLOWED_HOSTS = ['127.0.0.1',
                 'rubys-fitness-plan-f9d280e18cb5.herokuapp.com', 'localhost']
 
 
